a63aa3f18375a0c5a37bd9b72cdf1c6d2a0c5bfb
Lucas Abrigo <luca.mdq@gmail.com>
2023-08-21 19:20:21 -0300

    style (Text):modified to Hola, coder!

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textContainer}>App en Android Studio!</Text>
+      <Text style={styles.textContainer}>Hola, Coder!</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}> Button </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +16,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellowgreen",
+    backgroundColor: "#a3bcff",
     alignItems: "center",
     justifyContent: "center",
   },
   textContainer: {
     fontSize: 30,
-    color: "white",
+    color: "#eef2ff",
+  },
+  button: {
+    backgroundColor: "#487aff",
+    padding: 4,
+    marginTop: 30,
+    borderRadius: 10,
+  },
+  textButton: {
+    padding: 3,
+    fontSize: 20,
+    color: "#eef2ff",
   },
 });
